fa9132df5a30141dddef82650453ffea285de69c
Itolstoganov <oockvay@gmail.com>
2016-03-14 04:05:02 +0300

    stringSet serialize implementation

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -5,7 +5,9 @@ package ru.spbau.mit;
   on 26.02.2015
 */
 
-public class StringSetImpl implements StringSet {
+import java.io.*;
+
+public class StringSetImpl implements StringSet, StreamSerializable {
 
     private static final int CHILDREN_MAX = 52;          /* Latin alphabet length */
     private Node root = new Node();
@@ -43,11 +45,32 @@ public class StringSetImpl implements StringSet {
         return root.countPrefixFromNode(prefix);
     }
 
+    @Override
+    public void serialize(OutputStream out) {
+        DataOutputStream outStream = new DataOutputStream(out);
+        root.serializeFromNode(outStream);
+    }
+
+    @Override
+    public void deserialize(InputStream in) {
+        try (DataInputStream inStream = new DataInputStream(in)) {
+            if (inStream.available() != 0) {
+                root.deserializeFromNode(inStream);
+            }
+        } catch (IOException e) {
+            throw new SerializationException();
+        }
+    }
+
     private static int latinLetterToInt(char letter) {
         if (Character.isLowerCase(letter)) {
             return letter - 'a';
         }
         return Character.toLowerCase(letter) - 'a' + (CHILDREN_MAX / 2);
+    }
+
+    private static char intToLatinLetter(int index) {
+        return (char) (index + 'a');
     }
 
     /*
@@ -56,15 +79,15 @@ public class StringSetImpl implements StringSet {
     */
     private static final class Node {
         private boolean isWordEnd;
-        private Node[] children;
         private int size;
+        private Node[] children;
 
         private Node() {
             isWordEnd = false;
             children = new Node[CHILDREN_MAX];
             size = 0;
         }
-
+        
         private Node descent(String stringToBeChecked, int pos) {
             if (pos == stringToBeChecked.length()) {
                 return this;
@@ -121,6 +144,51 @@ public class StringSetImpl implements StringSet {
                 return node.getSize();
             }
             return 0;
+        }
+
+        private void serializeFromNode(DataOutputStream outStream) {
+            try {
+                outStream.writeBoolean(isWordEnd);
+                outStream.writeInt(size);
+                int numberOfChildren = numberOfChildren();
+                outStream.writeInt(numberOfChildren);
+
+                for (int i = 0; i < CHILDREN_MAX; i++) {
+                    if (children[i] != null) {
+                        outStream.writeChar(i);
+                        children[i].serializeFromNode(outStream);
+                    }
+                }
+            } catch (IOException e) {
+                throw new SerializationException();
+            }
+        }
+
+        private void deserializeFromNode(DataInputStream inStream) {
+            try {
+                isWordEnd = inStream.readBoolean();
+                size = inStream.readInt();
+                int numberOfChildren = inStream.readInt();
+
+                for (int i = 0; i < numberOfChildren; i++) {
+                    char letter = inStream.readChar();
+                    int index = latinLetterToInt(letter);
+                    children[index] = new Node();
+                    children[index].deserializeFromNode(inStream);
+                }
+            } catch (IOException e) {
+                throw new SerializationException();
+            }
+        }
+
+        private int numberOfChildren() {
+            int nodeChildren = 0;
+            for (int i = 0; i < CHILDREN_MAX; i++) {
+                if (children[i] != null) {
+                    nodeChildren++;
+                }
+            }
+            return nodeChildren;
         }
     }
 
